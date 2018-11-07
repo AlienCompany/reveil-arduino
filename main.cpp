1c66b1d9c0f6c517bb6aaf87a49f1f341d2ff1ed
Notraly <fernandez.coralie2001@gmail.com>
2018-11-07 23:03:03 +0100

    feat(lcd): display text REV-3

--- main.cpp
+++ main.cpp
@@ -3,52 +3,30 @@
 #include "CommunicationService.h"
 #include "Led.h"
 #include "Sensor.h"
-//#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 
-const uint8_t PIN_SENSOR = 2;
-const uint8_t PIN_LED1 = 3;
-const uint8_t PIN_LED2 = 3;
+
 
 //char* SERVER = "letterbox.notraly.fr";
 //IPAddress SERVER(192,168,1,20);
-CommunicationService *communicationService ;
-
-Led *led1;
-Led *led2;
-Sensor *sensor1;
-
+//CommunicationService *communicationService ;
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
     Serial.begin(9600);
 
-    led1 = new Led(PIN_LED1, 0);
-    led2 = new Led(PIN_LED2, 0);
-    sensor1 = new Sensor(PIN_SENSOR, 200);
-
-    pinMode(PIN_SENSOR, INPUT);
-    pinMode(PIN_LED1, OUTPUT);
-    pinMode(PIN_LED2, OUTPUT);
-
-    led1->init();
-    led2->init();
+    lcd.begin();
+    lcd.backlight();
+    lcd.home();
+    lcd.print("Bonjour");
 
 //    communicationService = CommunicationService::getInstance();
 //    communicationService->init();
 //    communicationService->setServer(SERVER);
-    delay(1);
+//    delay(1);
 }
 
 void loop() {
-
-    sensor1->checkChange();
-
-    if(sensor1->getState() == OPENED){
-        led1->setBrightness(255);
-        led2->setBrightness(0);
-    } else {
-        led1->setBrightness(0);
-        led2->setBrightness(255);
-    }
 
 //    communicationService->loop();
 
